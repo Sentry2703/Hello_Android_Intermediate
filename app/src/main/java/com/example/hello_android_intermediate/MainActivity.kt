84316efa9742a68da540840f387507cf391842a6
Sentry2703 <adetunjiadewoye1@gmail.com>
2022-12-22 00:52:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hello_android_intermediate/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hello_android_intermediate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val helloButton = findViewById<Button>(R.id.helloButton)
+        helloButton.setOnClickListener {
+            Toast.makeText(this, "Hello to You!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
